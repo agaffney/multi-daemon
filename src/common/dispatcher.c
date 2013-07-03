3c85d1c50573bc7deae494ca9fc8ad701fc34be2
Andrew Gaffney <andrew@agaffney.org>
2013-07-03 07:59:57 -0700

    More dispatcher progress

--- src/common/dispatcher.c
+++ src/common/dispatcher.c
@@ -2,16 +2,28 @@
 
 #include <stdlib.h>
 
-Dispatcher * Dispatcher_init()
+Dispatcher * Dispatcher_init(unsigned short int worker_model)
 {
 	Dispatcher * self = (Dispatcher *)calloc(1, sizeof(Dispatcher));
-
+	self->_worker_model = worker_model;
 	// Assign function pointers
 	self->destroy = _dispatcher_destroy;
+	self->add_listener = _dispatcher_add_listener;
 	return self;
 }
 
 void _dispatcher_destroy(Dispatcher * self)
 {
 	free(self);
+}
+
+int _dispatcher_add_listener(Dispatcher * self, Socket * sock, int (*callback)(Dispatcher *, Socket *))
+{
+	if (self->_listener_count >= _DISPATCHER_MAX_LISTENERS)
+	{
+		return 1;
+	}
+	self->_listeners[self->_listener_count] = sock;
+	self->_listener_count++;
+	return 0;
 }
--- src/common/dispatcher.h
+++ src/common/dispatcher.h
@@ -1,9 +1,22 @@
-struct _Dispatcher {
+#include "socket.h"
 
+#define _DISPATCHER_MAX_LISTENERS 100
+
+#define DISPATCHER_WORKER_MODEL_NONE     0
+#define DISPATCHER_WORKER_MODEL_POSTFORK 1
+#define DISPATCHER_WORKER_MODEL_PREFORK  2
+#define DISPATCHER_WORKER_MODEL_THREAD   3
+
+struct _Dispatcher {
+	Socket * _listeners[_DISPATCHER_MAX_LISTENERS];
+	int _listener_count;
+	int _worker_model;
 	void (*destroy)(struct _Dispatcher *);
+	int (*add_listener)(struct _Dispatcher *, Socket *, int (*callback)(struct _Dispatcher *, Socket *));
 };
 
 typedef struct _Dispatcher Dispatcher;
 
-Dispatcher * Dispatcher_init();
+Dispatcher * Dispatcher_init(unsigned short int);
 void _dispatcher_destroy(Dispatcher *);
+int _dispatcher_add_listener(Dispatcher *, Socket *, int (*callback)(Dispatcher *, Socket *));
