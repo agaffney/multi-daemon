8e84a76e57b97670be6223cc01a2c04f1bd11ece
Andrew Gaffney <andrew@agaffney.org>
2013-07-04 10:43:48 -0700

    A lot more progress on the dispatcher

--- src/common/dispatcher.c
+++ src/common/dispatcher.c
@@ -1,14 +1,23 @@
 #include "dispatcher.h"
 
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
-Dispatcher * Dispatcher_init(unsigned short int worker_model)
+Dispatcher * Dispatcher_init(int worker_model, int num_workers)
 {
 	Dispatcher * self = (Dispatcher *)calloc(1, sizeof(Dispatcher));
 	self->_worker_model = worker_model;
+	self->_num_workers = num_workers;
 	// Assign function pointers
 	self->destroy = _dispatcher_destroy;
 	self->add_listener = _dispatcher_add_listener;
+	self->run = _dispatcher_run;
+	self->worker_run = _dispatcher_worker_run;
+	self->build_listener_fdset = _dispatcher_build_listener_fdset;
+	self->poll_listeners = _dispatcher_poll_listeners;
+	self->find_listener = _dispatcher_find_listener;
 	return self;
 }
 
@@ -32,5 +41,138 @@ int _dispatcher_add_listener(Dispatcher * self, Socket * sock, int (*callback)(D
 	listener->callback = callback;
 	self->_listeners[self->_listener_count] = listener;
 	self->_listener_count++;
+	return 0;
+}
+
+int _dispatcher_run(Dispatcher * self)
+{
+	switch (self->_worker_model)
+	{
+		case DISPATCHER_WORKER_MODEL_NONE:
+			self->worker_run(self, 1);
+			break;
+		case DISPATCHER_WORKER_MODEL_POSTFORK:
+			self->worker_run(self, 1);
+			break;
+		case DISPATCHER_WORKER_MODEL_PREFORK:
+			// Create a mutex/semaphore, fork off workers, then call worker_run()
+			break;
+		case DISPATCHER_WORKER_MODEL_THREAD:
+			// Create a mutex/semaphore and start threads with worker_run()
+			break;
+		default:
+			return 1;
+	}
+	return 0;
+}
+
+int _dispatcher_build_listener_fdset(Dispatcher * self, fd_set * rfds)
+{
+	int max_fd;
+
+	FD_ZERO(rfds);
+	for (int i = 0; i < self->_listener_count; i++)
+	{
+		FD_SET(self->_listeners[i]->sock->socket, rfds);
+		if (self->_listeners[i]->sock->socket > max_fd)
+		{
+			max_fd = self->_listeners[i]->sock->socket;
+		}
+	}
+
+	return max_fd;
+}
+
+int _dispatcher_poll_listeners(Dispatcher * self, fd_set * rfds, int max_fd)
+{
+	struct timeval *timeout = (struct timeval *)calloc(1, sizeof(struct timeval));
+	timeout->tv_sec = 0;
+	timeout->tv_usec = 0;
+
+	int n = select(max_fd + 1, rfds, NULL, NULL, timeout);
+
+	free(timeout);
+
+	return n;
+
+/*
+	if (n < 0)
+	{
+		printf("_dispatcher_poll_listeners(): %s\n", strerror(errno));
+		return -1;
+	}
+	if (n == 0)
+	{
+		// Timeout
+	}
+	if (FD_ISSET(self->socket, &rfds))
+	{
+		return 1;
+	}
+*/
+}
+
+dispatcher_listener * _dispatcher_find_listener(Dispatcher * self, int socket_fd)
+{
+	for (int i = 0; i < self->_listener_count; i++)
+	{
+		if (self->_listeners[i]->sock->socket == socket_fd)
+		{
+			return self->_listeners[i];
+		}
+	}
+	return NULL;
+}
+
+int _dispatcher_worker_run(Dispatcher * self, int worker_num)
+{
+	fd_set * rfds = (fd_set *)calloc(1, sizeof(fd_set));
+	int max_fd = self->build_listener_fdset(self, rfds);
+
+	switch (self->_worker_model)
+	{
+		case DISPATCHER_WORKER_MODEL_NONE:
+		case DISPATCHER_WORKER_MODEL_POSTFORK:
+			while (1)
+			{
+				int ready_fds, child_status;
+				int last_ready_fd = -1;
+				// Naively clean up after children
+				waitpid(-1, &child_status, WNOHANG);
+				// Look for sockets that are ready for action
+				ready_fds = self->poll_listeners(self, rfds, max_fd);
+				if (ready_fds <= 0)
+				{
+					continue;
+				}
+				// Figure out which listeners are ready
+				for (int i = 0; i < ready_fds; i++)
+				{
+					for (int j = last_ready_fd + 1; j <= max_fd; j++)
+					{
+						
+						if (FD_ISSET(j, rfds))
+						{
+							last_ready_fd = j;
+							dispatcher_listener * tmp_listener = self->find_listener(self, j);
+							if (self->_worker_model == DISPATCHER_WORKER_MODEL_POSTFORK)
+							{
+								// fork it
+								pid_t child_pid = fork();
+								if (child_pid > 0)
+								{
+									break;
+								}
+							}
+							tmp_listener->callback(self, tmp_listener->sock);							
+							break;
+						}
+					}
+
+				}
+			}
+			break;
+	}
+
 	return 0;
 }
--- src/common/dispatcher.h
+++ src/common/dispatcher.h
@@ -1,5 +1,7 @@
 #include "socket.h"
 
+#include <sys/select.h>
+
 #define _DISPATCHER_MAX_LISTENERS 100
 
 #define DISPATCHER_WORKER_MODEL_NONE     0
@@ -11,8 +13,14 @@ struct _Dispatcher {
 	struct _dispatcher_listener * _listeners[_DISPATCHER_MAX_LISTENERS];
 	int _listener_count;
 	int _worker_model;
+	int _num_workers;
 	void (*destroy)(struct _Dispatcher *);
 	int (*add_listener)(struct _Dispatcher *, Socket *, int (*callback)(struct _Dispatcher *, Socket *));
+	int (*run)(struct _Dispatcher *);
+	int (*worker_run)(struct _Dispatcher *, int);
+	int (*build_listener_fdset)(struct _Dispatcher *, fd_set *);
+	int (*poll_listeners)(struct _Dispatcher *, fd_set *, int);
+	struct _dispatcher_listener * (*find_listener)(struct _Dispatcher *, int);
 };
 
 typedef struct _Dispatcher Dispatcher;
@@ -24,6 +32,12 @@ struct _dispatcher_listener {
 
 typedef struct _dispatcher_listener dispatcher_listener;
 
-Dispatcher * Dispatcher_init(unsigned short int);
+Dispatcher * Dispatcher_init(int, int);
 void _dispatcher_destroy(Dispatcher *);
 int _dispatcher_add_listener(Dispatcher *, Socket *, int (*callback)(Dispatcher *, Socket *));
+int _dispatcher_run(Dispatcher *);
+int _dispatcher_worker_run(Dispatcher *, int);
+int _dispatcher_build_listener_fdset(Dispatcher *, fd_set *);
+int _dispatcher_poll_listeners(Dispatcher *, fd_set *, int);
+dispatcher_listener * _dispatcher_find_listener(Dispatcher *, int);
+
