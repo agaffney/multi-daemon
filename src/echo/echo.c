2bfd055ca3a66c208802688335c486b971f28eff
Andrew Gaffney <andrew@agaffney.org>
2013-07-05 19:24:15 -0700

    First pass at converting echo to dispatcher

--- src/echo/echo.c
+++ src/echo/echo.c
@@ -11,9 +11,8 @@
 
 int echo_entry(config_opt config_opts[])
 {
-	int i, port;
+	int i, port, socket_type, worker_model, num_workers;
 	char proto[5];
-	int (*recv_ready_callback)();
 
 	for(i=0;;i++)
 	{
@@ -37,15 +36,48 @@ int echo_entry(config_opt config_opts[])
 			proto[sizeof(proto)-1] = 0;
 			if (!strcmp(proto, "tcp"))
 			{
-				recv_ready_callback = echo_recv_ready_tcp;
+				socket_type = SOCK_STREAM;
 			}
 			else if (!strcmp(proto, "udp"))
 			{
-				recv_ready_callback = echo_recv_ready_udp;
+				socket_type = SOCK_DGRAM;
 			}
 			else
 			{
 				fprintf(stderr, "Invalid proto specification '%s'. Must be one of: tcp, udp\n", config_opts[i].value);
+				return 1;
+			}
+		}
+		else if (!strcmp(config_opts[i].name, "worker_model"))
+		{
+			if (!strcmp(config_opts[i].value, "single"))
+			{
+				worker_model = DISPATCHER_WORKER_MODEL_SINGLE;
+			}
+			else if (!strcmp(config_opts[i].value, "postfork"))
+			{
+				worker_model = DISPATCHER_WORKER_MODEL_POSTFORK;
+			}
+			else if (!strcmp(config_opts[i].value, "prefork"))
+			{
+				worker_model = DISPATCHER_WORKER_MODEL_PREFORK;
+			}
+			else if (!strcmp(config_opts[i].value, "thread"))
+			{
+				worker_model = DISPATCHER_WORKER_MODEL_THREAD;
+			}
+			else
+			{
+				fprintf(stderr, "Invalid worker_model specification: %s\n", config_opts[i].value);
+				return 1;
+			}
+		}
+		else if (!strcmp(config_opts[i].name, "num_workers"))
+		{
+			num_workers = atoi(config_opts[i].value);
+			if (num_workers <= 0 || num_workers > 200)
+			{
+				fprintf(stderr, "Invalid num_workers specification: %s\n", config_opts[i].value);
 				return 1;
 			}
 		}
@@ -56,15 +88,50 @@ int echo_entry(config_opt config_opts[])
 		}
 	}
 
-	server_info *srv_info = (server_info *)calloc(1, sizeof(server_info));
-	strcpy(srv_info->proto, proto);
-	srv_info->port = port;
-	srv_info->recv_ready_callback = recv_ready_callback;
+	Socket *sock = Socket_init(0);
+	sock->create(sock, AF_INET, socket_type);
+	if (sock == NULL)
+	{
+		fprintf(stderr, "Failed to create socket\n");
+		return 1;
+	}
 
-	i = server_start(srv_info);
+	sock->set_flag(sock, O_NONBLOCK);
+	if (sock->bind(sock, "0.0.0.0", port) < 0)
+	{
+		fprintf(stderr, "Bind to socket failed\n");
+		return 1;
+	}
+	if (socket_type == SOCK_STREAM)
+	{
+		if (sock->listen(sock, 5) < 0)
+		{
+			fprintf(stderr, "Listen on socket failed\n");
+			return 1;
+		}
+	}
 
-	free(srv_info);
+	Dispatcher * disp = Dispatcher_init(worker_model, num_workers);
+	disp->add_listener(disp, sock, echo_dispatcher_callback);
+	disp->run(disp);
 
+	disp->destroy(disp);
+	sock->destroy(sock);
+
+	return 0;
+}
+
+int echo_dispatcher_callback(Dispatcher * dispatcher, Socket * sock)
+{
+	switch (sock->type)
+	{
+		case SOCK_DGRAM:
+			return echo_recv_ready_udp(sock);
+			break;
+		case SOCK_STREAM:
+			return echo_recv_ready_tcp(sock);
+			break;
+	}
 	return 0;
 }
 
--- src/echo/echo.h
+++ src/echo/echo.h
@@ -1,7 +1,8 @@
 #include "common/socket.h"
-#include "common/server.h"
+#include "common/dispatcher.h"
 
 int echo_entry(config_opt[]);
+int echo_dispatcher_callback(Dispatcher *, Socket *);
 int echo_recv_ready_udp(Socket *);
 int echo_recv_ready_tcp(Socket *);
 
